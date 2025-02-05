50a705beafa161ed4227b51dab830a57f3790155
niclas-muller <nicmuell@outlook.com>
2025-02-05 01:39:33 +0100

    ...

--- python/main.py
+++ python/main.py
@@ -1,14 +1,62 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from shanks import ShanksConverge
 
 class ContinuedFunctionArtFrame():
-    def __init__():
-        pass
+    # For a given function, center point, extent and resolution, compute the frame
+    def __init__(self, function, center, extent, resolution, grid=None):
+        self.function = function
+        if grid:
+            self.grid = grid
+        else:
+            self.constructGrid(center, extent, resolution)
+        self.computeFrame()
+
+    def constructGrid(self, center, extent, resolution):
+        lowerLeftCorner = (center[0] - 0.5*extent, center[1] - 0.5*extent)
+        upperRghtCorner = (center[0] + 0.5*extent, center[1] + 0.5*extent)
+        xGrid = np.linspace(lowerLeftCorner[0], upperRghtCorner[0], num=resolution)
+        yGrid = np.linspace(lowerLeftCorner[1], upperRghtCorner[1], num=resolution)
+        self.grid = np.empty((resolution, resolution), dtype = np.complex128)
+        for xCount, x in enumerate(xGrid):
+            for yCount, y in enumerate(yGrid):
+                self.grid[xCount,yCount] = x + 1.j*y
+
+    def computeFrame(self):
+        
+        def applyContinuedFunction(z):
+            shnks = ShanksConverge(z,self.function)
+            if shnks.isConverged:
+                return np.angle(shnks.limit)
+            else: 
+                return np.nan
+
+        vectorizedApply = np.vectorize(applyContinuedFunction)
+        self.frame = vectorizedApply(self.grid)
 
 class ContinuedFunctionArtWalk():
-    def __init__():
+    def __init__(self):
         pass
 
 class ContinuedFunctionArtZoom():
-    def __init__():
+    def __init__(self):
         pass
+
+center = (0,0)
+extent = 1
+resolution = 500
+
+for _ in range(50):
+    randoms = 2*(np.random.rand(4)-0.5)
+    a = randoms[0] + 1.j*randoms[1]
+    b = randoms[2] + 1.j*randoms[3]
+    func = lambda z: np.log(a+b*np.sin(z)/z)
+    frame = ContinuedFunctionArtFrame(func, center, extent, resolution).frame
+    fig, ax = plt.subplots(1)
+    ax.imshow(frame,origin='lower',cmap='twilight')
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.set_xticks([])
+    ax.set_yticks([])
+    fig.savefig(f'../images/favicons/sample_{_}.png', bbox_inches='tight')
+
--- python/shanks.py
+++ python/shanks.py
@@ -1,7 +1,7 @@
 class ShanksConverge():
     # Compute the limit of a sequence defined by continued application of the function "function" to the starting value initVal using all available shanks transforms
 
-    def __init__(initVal,function):
+    def __init__(self,initVal,function):
         # initialize
         self.function = function
         self.shanksLadder = [[initVal]]
@@ -11,33 +11,38 @@ class ShanksConverge():
         self.shanksLevel = 0
         self.isConverged = False
         self.maxIterations = 100
-        self.convergenceThreshold = 1e-3
-        self.bestEstimates = [initVal]
+        self.convergenceThreshold = 1e-4
+        self.bestEstimates = []
         self.iterate()
 
     def iterateBase(self):
         # apply the function to the last element of the base sequence (without any shanks transforms)
-        self.shanksLadder[0].append(function(self.shanksLadder[0][-1]))
+        self.shanksLadder[0].append(self.function(self.shanksLadder[0][-1]))
         self.totalIterations += 1
 
     def iterateShanks(self):
         for depth in range(self.shanksLevel):
-            tmp = self.shanksLadder[depth][-2:]
+            tmp = self.shanksLadder[depth][-3:]
             self.shanksLadder[depth + 1].append((tmp[0]*tmp[2] - tmp[1]*tmp[1])/(tmp[0] + tmp[2] - 2*tmp[1]))
 
     def increaseShanksLevel(self):
         # increase the shanks depth by one level, compute the first element in the deepest level
         self.shanksLadder.append([])
         self.shanksLevel += 1
-        tmp = self.shanksLadder[-2][-2:]
+        tmp = self.shanksLadder[-2][-3:]
         self.shanksLadder[-1].append((tmp[0]*tmp[2] - tmp[1]*tmp[1])/(tmp[0] + tmp[2] - 2*tmp[1]))
 
     def assessConvergence(self):
         # add the latest approximant to the list of limits and check if convergence has been achieved
         self.bestEstimates.append(self.shanksLadder[-1][-1])
-        if abs(self.bestEstimates[-1] - self.bestEstimates[-2]) < self.convergenceThreshold:
-            self.isConverged = True
-            self.limit = self.bestEstimates[-1]
+        if len(self.bestEstimates) < 3:
+            pass
+        else:
+            diff1 = abs(self.bestEstimates[-1] - self.bestEstimates[-2])
+            diff2 = abs(self.bestEstimates[-2] - self.bestEstimates[-3])
+            if diff1 < self.convergenceThreshold and diff2 < self.convergenceThreshold:
+                self.isConverged = True
+                self.limit = self.bestEstimates[-1]
 
     def iterate(self):
         while not self.isConverged and self.totalIterations < self.maxIterations:
